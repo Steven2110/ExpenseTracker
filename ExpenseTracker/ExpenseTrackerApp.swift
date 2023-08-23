cdd866cfa4f4a3542bc79a281d0c0db77c9c6246
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-08-23 16:42:10 +0700

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Steven Wijaya on 23.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
