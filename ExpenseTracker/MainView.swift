12a8ec401825394ad96b9469f99d7cfcd2b341c5
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-08-23 20:53:55 +0700

    Added assets for color, extension for color, and change ContentView to MainView

---
+++ ExpenseTracker/MainView.swift
@@ -0,0 +1,25 @@
+//
+//  MainView.swift
+//  ExpenseTracker
+//
+//  Created by Steven Wijaya on 23.08.2023.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                
+            }
+        }
+        .padding()
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ ExpenseTracker/Utilities/Extension/Color+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Ext.swift
+//  ExpenseTracker
+//
+//  Created by Steven Wijaya on 23.08.2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+    static let brandBackground = Color("brandBackground")
+    static let textColor = Color("textColor")
+    static let primaryAccent = Color("primaryAccent")
+    static let secondaryAccent = Color("secondaryAccent")
+    static let buttonColor = Color("buttonColor")
+    static let linkColor = Color("linkColor")
+}
