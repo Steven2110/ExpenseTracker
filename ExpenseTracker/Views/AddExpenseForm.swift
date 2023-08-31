77b62187d6de8214e84c8a33542cb4d9d6849e8d
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-09-01 01:09:16 +0700

    Embed Form in AddExpenseForm inside NavigationStack

--- ExpenseTracker/Views/AddExpenseForm.swift
+++ ExpenseTracker/Views/AddExpenseForm.swift
@@ -16,55 +16,58 @@ struct AddExpenseForm: View {
     @State private var transactionDate: Date = Date()
 
     var body: some View {
-        Form {
-            LabeledContent {
-                TextField("Expense name", text: $name).multilineTextAlignment(.trailing)
-            } label: {
-                Text("Expense name")
-            }
-            CategoryPicker(category: $category)
-            LabeledContent {
-                TextField("0.00", text: $amountStr)
-                    .fixedSize()
-                    .multilineTextAlignment(.trailing)
-                    .keyboardType(.decimalPad)
-            } label: {
-                Text("Expense amount ($)")
-            }
-            VStack {
-                Text("Transaction date").frame(maxWidth: .infinity, alignment: .leading)
-                DatePicker("Transaction date", selection: $transactionDate).datePickerStyle(.graphical)
-            }
-            Section {
-                Button("Add") {
-                    guard checkValidity() else {
-                        // -TODO: Show alert
-                        print("Missing one or all of the value in text field.")
-                        return
+        NavigationStack {
+            Form {
+                LabeledContent {
+                    TextField("Expense name", text: $name).multilineTextAlignment(.trailing)
+                } label: {
+                    Text("Expense name")
+                }
+                CategoryPicker(category: $category)
+                LabeledContent {
+                    TextField("0.00", text: $amountStr)
+                        .fixedSize()
+                        .multilineTextAlignment(.trailing)
+                        .keyboardType(.decimalPad)
+                } label: {
+                    Text("Expense amount ($)")
+                }
+                VStack {
+                    Text("Transaction date").frame(maxWidth: .infinity, alignment: .leading)
+                    DatePicker("Transaction date", selection: $transactionDate).datePickerStyle(.graphical)
+                }
+                Section {
+                    Button("Add") {
+                        guard checkValidity() else {
+                            // -TODO: Show alert
+                            print("Missing one or all of the value in text field.")
+                            return
+                        }
+                        
+                        let formatter = NumberFormatter()
+                        formatter.decimalSeparator = ","
+                        
+                        guard let formattedValue = formatter.number(from: amountStr) else {
+                            // -TODO: Show alert
+                            print("Error value of expense amount")
+                            return
+                        }
+                        let amount = Double(truncating: formattedValue)
+                        
+                        let expense = Expense(name: name, category: category, date: transactionDate, amount: amount)
+                        
+                        expenses.append(expense)
                     }
-                    
-                    let formatter = NumberFormatter()
-                    formatter.decimalSeparator = ","
-                    
-                    guard let formattedValue = formatter.number(from: amountStr) else {
-                        // -TODO: Show alert
-                        print("Error value of expense amount")
-                        return
-                    }
-                    let amount = Double(truncating: formattedValue)
-                    
-                    let expense = Expense(name: name, category: category, date: transactionDate, amount: amount)
-                    
-                    expenses.append(expense)
                 }
             }
-        }
-        .toolbar {
-            ToolbarItemGroup(placement: .keyboard) {
-                Spacer()
-                
-                Button(action: dismissKeyboard) {
-                    Image(systemName: "keyboard.chevron.compact.down")
+            .navigationTitle("Add Expense")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button(action: dismissKeyboard) {
+                        Image(systemName: "keyboard.chevron.compact.down")
+                    }
                 }
             }
         }
